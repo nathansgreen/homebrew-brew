d1fff3dcbdf74bb28dc680182b68d3a4f556c3c4
nathansgreen <nathansgreen@users.noreply.github.com>
2014-08-07 14:31:15 -0500

    Create checker-framework.rb

---
+++ checker-framework.rb
@@ -0,0 +1,7 @@
+require 'formula'
+
+class CheckerFramework < Formula
+  homepage 'http://types.cs.washington.edu/checker-framework/'
+  url 'http://types.cs.washington.edu/checker-framework/current/checker-framework.zip'
+  sha1 '7cb7164e68e010a74f0182153cb11b01318c02c5'
+end
